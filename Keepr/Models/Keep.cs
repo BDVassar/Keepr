9f7321ab9fda4183d75df500e6bc403579be4e52
Bryan Vassar <vassar.bryan@gmail.com>
2023-01-27 12:54:40 -0700

    Models all created, profile class created account ext profile

---
+++ Keepr/Models/Keep.cs
@@ -0,0 +1,11 @@
+namespace Keepr.Models;
+
+public class Keep
+{
+  public int Id { get; set; }
+  public string CreatroId { get; set; }
+  public string Name { get; set; }
+  public string Description { get; set; }
+  public string img { get; set; }
+  public int views { get; set; }
+}
---
+++ Keepr/Models/VaultKeeps.cs
@@ -0,0 +1,9 @@
+namespace Keepr.Models;
+
+public class VaultKeeps
+{
+  public int Id { get; set; }
+  public string CreatorId { get; set; }
+  public int VaultId { get; set; }
+  public int KeepId { get; set; }
+}
